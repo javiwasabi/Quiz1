c6974f03ecc8bb81f0e2a2a92fd6831cb3b814ff
javiwasabi <javiera.osoriom@usm.cl>
2025-01-29 09:06:38 -0300

    Quiz-2 fixed traductions

--- quiz-2/src/components/buttons.tsx
+++ quiz-2/src/components/buttons.tsx
@@ -1,20 +1,8 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
-export const ButtonStart: React.FC = () => {
-  return (
-    <div className="flex flex-col items-center justify-center h-[10%] w-full font-title">
-      <Link to="/question">
-        <button
-          className="relative -top-1 -left-1 bg-gray-800 py-10 px-8 sm:py-20 sm:px-10 font-medium uppercase text-white text-3xl sm:text-2xl bg-transparent"
-          style={{ transform: "rotate(0deg)" }}
-        >
-          START
-        </button>
-      </Link>
-    </div>
-  );
-};
+const userLang = navigator.language || navigator.language;
+const isSpanish = userLang.startsWith("es");
 
 interface ButtonNextProps {
   onClick?: () => void;
@@ -30,12 +18,11 @@ export const ButtonNext: React.FC<ButtonNextProps> = ({ onClick, id }) => {
         onClick={onClick}
       >
         <div className="absolute inset-0 opacity-0 hover:opacity-100 transition-opacity"></div>
-        <span className="relative z-10">NEXT</span>
+        <span className="relative z-10">{isSpanish ? "SIGUIENTE" : "NEXT"}</span>
       </button>
     </div>
   );
 };
-
 
 export const Choice: React.FC<{ 
   onClick: () => void; 
@@ -48,18 +35,15 @@ export const Choice: React.FC<{
         className={`bg-white flex items-center justify-center w-80 py-3 px-4 sm:py-4 sm:px-6 md:py-5 md:px-8 lg:py-6 lg:px-10 font-medium uppercase text-black text-xl sm:text-xl md:text-xl lg:text-2xl xl:text-3xl leading-tight rounded-full shadow-lg hover:shadow-2xl transition-shadow duration-300 ${
           disabled ? "opacity-50 cursor-not-allowed" : "hover:bg-gray-200"
         }`}
-        onClick={!disabled ? onClick : undefined} // Evita ejecutar onClick si está deshabilitado
+        onClick={!disabled ? onClick : undefined}
         id={id}
-        disabled={disabled} // HTML prop para deshabilitar el botón
+        disabled={disabled}
       >
-        {id === "choice-pokemon" ? "Pokemon" : "Technology"}
+        {id === "choice-pokemon" ? "Pokémon" : isSpanish ? "Tecnología" : "Technology"}
       </button>
     </div>
   );
 };
-
-
-
 
 export const ButtonStartp: React.FC = () => {
   return (
@@ -76,26 +60,25 @@ export const ButtonStartp: React.FC = () => {
             alt="Pokeball"
             className="w-8 h-8 sm:w-12 sm:h-12 object-contain z-10 mr-2"
           />
-          START
+          {isSpanish ? "INICIAR" : "START"}
         </button>
       </Link>
     </div>
   );
 };
 
-
 export const NextP: React.FC<ButtonNextProps> = ({ onClick, id }) => {
   return (
     <div className="flex flex-col items-center justify-center h-[10%] w-full font-bentham">
       <button
-        className="flex items-center justify-center  text-white py-4 px-6 sm:py-6 sm:px-8 font-semibold uppercase text-2xl sm:text-3xl transition-transform duration-300 ease-in-out hover:scale-105 focus:outline-none focus:ring-4 focus:ring-blue-500 rounded-lg shadow-lg"
+        className="flex items-center justify-center text-white py-4 px-6 sm:py-6 sm:px-8 font-semibold uppercase text-2xl sm:text-3xl transition-transform duration-300 ease-in-out hover:scale-105 focus:outline-none focus:ring-4 focus:ring-blue-500 rounded-lg shadow-lg"
         style={{ transform: "rotate(0deg)" }}
         onClick={onClick}
         id={id}
         aria-label="Next Button"
       >
         <div className="absolute inset-0 bg-black opacity-60 rounded-lg shadow-x z-0"></div>
-        <span className="mr-4 z-10">NEXT</span>
+        <span className="mr-4 z-10">{isSpanish ? "SIGUIENTE" : "NEXT"}</span>
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Pok%C3%A9_Ball_icon.svg/640px-Pok%C3%A9_Ball_icon.svg.png"
           alt="Pokeball"
@@ -109,13 +92,12 @@ export const NextP: React.FC<ButtonNextProps> = ({ onClick, id }) => {
 interface PokemonCardProps {
   onClick: () => void;
   id: string;
-}export const PokemonCard: React.FC<PokemonCardProps> = ({ onClick, id }) => {
-  const userLanguage = navigator.language || navigator.languages[0];
-  const isSpanish = userLanguage.startsWith("es");
+}
 
+export const PokemonCard: React.FC<PokemonCardProps> = ({ onClick, id }) => {
   // Función para manejar el clic en el ícono de la Pokébola
   const handleClick = () => {
-    onClick(); // Activar la acción del clic
+    onClick();
   };
 
   return (
@@ -126,8 +108,8 @@ interface PokemonCardProps {
         left: "50%",
         transform: "translateX(-50%)",
         zIndex: 9999,
-        width: "50%", // Hacer el deslizable más corto
-        maxWidth: "200px", // Limitar el tamaño máximo
+        width: "50%",
+        maxWidth: "200px",
         height: "60px",
         borderRadius: "30px",
         backgroundColor: "#ddd",
@@ -136,9 +118,9 @@ interface PokemonCardProps {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        cursor: "pointer", // Cambiar el cursor al hacer hover
+        cursor: "pointer",
       }}
-      onClick={handleClick} // Activar la acción con un clic en todo el contenedor
+      onClick={handleClick}
     >
       <div
         style={{
