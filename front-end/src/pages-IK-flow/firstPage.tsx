ac300179f8e0bcdc1482889d91e121d659f00f26
javiwasabi <javiera.osoriom@usm.cl>
2025-01-30 09:21:39 -0300

    Quiz 1 fixed

--- front-end/src/pages-IK-flow/firstPage.tsx
+++ front-end/src/pages-IK-flow/firstPage.tsx
@@ -34,7 +34,7 @@ const Vie: React.FC = () => {
       className="absolute inset-0 h-[97%] justify-center items-center w-full object-cover z-0"  
     />
 
-<div className="absolute top-[8%] text-center z-20 w-[85%] h-[23vh] flex items-center justify-center px-4 bg-transparent shadow-xl"> 
+<div className="absolute top-[12%] text-center z-20 w-[85%] h-[23vh] flex items-center justify-center px-4 bg-transparent shadow-xl"> 
   <p className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold font-bentham text-black break-words">
     {t("guessText")}
   </p>
