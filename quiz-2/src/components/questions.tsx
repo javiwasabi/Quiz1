c15cbe49a52dcb4eb204d2f6ff55190da18e701b
javiwasabi <javiera.osoriom@usm.cl>
2025-01-31 11:28:46 -0300

    Quiz 2 fixed

--- quiz-2/src/components/questions.tsx
+++ quiz-2/src/components/questions.tsx
@@ -52,7 +52,7 @@ export const CardPok: React.FC<CardPropsp> = ({
       <div className="relative flex flex-col items-center font-bentham w-full">
         {/* Ball */}
         <div
-  className="fixed top-[-20%] sm:top-[-10%] w-40 h-40 ball"
+  className="fixed top-[-20%] sm:top-[-10%] w-40 h-40 ball z-0"
   style={{
     transform: isRevealed ? "rotate(0deg)" : "rotateY(180deg)",
     transition: "transform 0.5s ease-in-out",
@@ -83,7 +83,7 @@ export const CardPok: React.FC<CardPropsp> = ({
               opacity: isRevealed ? "1" : "0",
               top: "6vh",
             }}
-            className="card w-full max-w-[210px] sm:max-w-[250px] md:max-w-[300px] lg:max-w-[350px] xl:max-w-[250px] h-[200px] sm:h-[180px] md:h-[160px] lg:h-[140px] xl:h-[120px]" // Ajuste de la altura
+            className="card w-full max-w-[210px] sm:max-w-[250px] md:max-w-[300px] lg:max-w-[350px] xl:max-w-[250px] h-[200px] sm:h-[180px] md:h-[160px] lg:h-[140px] xl:h-[120px] z-10" // Ajuste de la altura
           >
             <img
               src={imageUrl}
